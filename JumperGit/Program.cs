93d79e9c2731d9d377337eb131d3a80efbd13252
pakokord <pakokord@hotmail.de>
2023-12-15 16:50:28 +0100

    Changed to Germany

--- JumperGit/Program.cs
+++ JumperGit/Program.cs
@@ -10,7 +10,7 @@ namespace JumperGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Germany");
         }
     }
 }
